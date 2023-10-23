65119c5a97e715b8fa611aeea5211043165e86d9
anxpara <anxpara@gmail.com>
2023-10-22 18:47:19 -0700

    config trialgrounds and playwright

--- trialgrounds/vite.config.ts
+++ trialgrounds/vite.config.ts
@@ -2,8 +2,21 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [sveltekit()],
+
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}'],
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "src/variables.scss" as *;',
+      },
+
+      sass: {
+        additionalData: '@use "src/variables.sass" as *',
+      },
+    },
+  },
 });
