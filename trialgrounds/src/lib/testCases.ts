e63ee7497fd94e7c6dd727c8bb1a3a9e9c34b57b
anxpara <anxpara@gmail.com>
2023-10-24 18:09:07 -0700

    move Test instantiations into allTestCases list

--- trialgrounds/src/lib/testCases.ts
+++ trialgrounds/src/lib/testCases.ts
@@ -35,108 +35,27 @@ export type Test = {
   matcher?: Matcher;
 };
 
-export const controlTestCase: Test = {
-  name: TestName.Control,
-  testType: Control,
-};
-export const subPixelTestCase: Test = {
-  name: TestName.SubPixel,
-  testType: SubPixel,
-};
-export const relativeTestCase: Test = {
-  name: TestName.Relative,
-  testType: Relative,
-};
-export const absoluteTestCase: Test = {
-  name: TestName.Absolute,
-  testType: Absolute,
-};
-export const fixedTestCase: Test = {
-  name: TestName.Fixed,
-  testType: Fixed,
-};
-export const fixedInTransformTestCase: Test = {
-  name: TestName.FixedInTransform,
-  testType: FixedInTransform,
-};
-export const scrollTestCase: Test = {
-  name: TestName.Scroll,
-  testType: Scroll,
-};
-export const scrollInRotateTestCase: Test = {
-  name: TestName.ScrollInRotate,
-  testType: ScrollInRotate,
-};
-export const stickyTestCase: Test = {
-  name: TestName.Sticky,
-  testType: Sticky,
-};
-export const translatedTestCase: Test = {
-  name: TestName.Translated,
-  testType: Translated,
-};
-export const rotatedTestCase: Test = {
-  name: TestName.Rotated,
-  testType: Rotated,
-};
-export const parentRotatedTestCase: Test = {
-  name: TestName.ParentRotated,
-  testType: ParentRotated,
-};
-export const twoRotationsTestCase: Test = {
-  name: TestName.TwoRotations,
-  testType: TwoRotations,
-};
-export const rotationsOrigin0TestCase: Test = {
-  name: TestName.RotationsOrigin0,
-  testType: Origin0Rotations,
-};
-export const rotations3dTestCase: Test = {
-  name: TestName.Rotations3d,
-  testType: Rotations3d,
-};
-export const rotationsPreserve3dTestCase: Test = {
-  name: TestName.RotationsPreserve3d,
-  testType: RotationsPreserve3d,
-};
-export const rotations3dCounteringTestCase: Test = {
-  name: TestName.Rotations3dCountering,
-  testType: Rotations3dCountering,
-};
-export const rotationsPreserve3dCounteringTestCase: Test = {
-  name: TestName.RotationsPreserve3dCountering,
-  testType: RotationsPreserve3dCountering,
-};
-export const transforms3dComplicatedTestCase: Test = {
-  name: TestName.Transforms3dComplicated,
-  testType: Transforms3dComplicated,
-};
-export const transformsPreserve3dComplicatedTestCase: Test = {
-  name: TestName.TransformsPreserve3dComplicated,
-  testType: TransformsPreserve3dComplicated,
-};
-
-export const allTestCases = [
-  controlTestCase,
-  subPixelTestCase,
-  relativeTestCase,
-  absoluteTestCase,
-  fixedTestCase,
-  fixedInTransformTestCase,
-  scrollTestCase,
-  scrollInRotateTestCase,
-  stickyTestCase,
-  translatedTestCase,
-  rotatedTestCase,
-  parentRotatedTestCase,
-  twoRotationsTestCase,
-  rotationsOrigin0TestCase,
-  rotations3dTestCase,
-  rotationsPreserve3dTestCase,
-  rotations3dCounteringTestCase,
-  rotationsPreserve3dCounteringTestCase,
-  transforms3dComplicatedTestCase,
-  transformsPreserve3dComplicatedTestCase,
+export const allTestCases: Test[] = [
+  { name: TestName.Control, testType: Control },
+  { name: TestName.SubPixel, testType: SubPixel },
+  { name: TestName.Relative, testType: Relative },
+  { name: TestName.Absolute, testType: Absolute },
+  { name: TestName.Fixed, testType: Fixed },
+  { name: TestName.FixedInTransform, testType: FixedInTransform },
+  { name: TestName.Scroll, testType: Scroll },
+  { name: TestName.ScrollInRotate, testType: ScrollInRotate },
+  { name: TestName.Sticky, testType: Sticky },
+  { name: TestName.Translated, testType: Translated },
+  { name: TestName.Rotated, testType: Rotated },
+  { name: TestName.ParentRotated, testType: ParentRotated },
+  { name: TestName.TwoRotations, testType: TwoRotations },
+  { name: TestName.RotationsOrigin0, testType: Origin0Rotations },
+  { name: TestName.Rotations3d, testType: Rotations3d },
+  { name: TestName.RotationsPreserve3d, testType: RotationsPreserve3d },
+  { name: TestName.Rotations3dCountering, testType: Rotations3dCountering },
+  { name: TestName.RotationsPreserve3dCountering, testType: RotationsPreserve3dCountering },
+  { name: TestName.Transforms3dComplicated, testType: Transforms3dComplicated },
+  { name: TestName.TransformsPreserve3dComplicated, testType: TransformsPreserve3dComplicated },
 ];
 export const testCasesByName = new Map<TestName, Test>(
   allTestCases.map((test) => [test.name, test]),
